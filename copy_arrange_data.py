fa71f48c196803fb3a51e9fa863b73690bf9970c
zhengsiy <18046653170@163.com>
2023-08-17 18:00:08 +0800

    处理埋点数据结果

--- copy_arrange_data.py
+++ copy_arrange_data.py
@@ -4,58 +4,81 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
 
 
-def copy_arrange_data():
 
-    def ATMlog_data():
+def ATMlog_data():
 
-        # 读取 excel 文件
-        excel_file = "I:/Buried-point-testing/ATMlog.xlsx"
+    # 读取源 excel 文件
+    excel_file = "I:/Buried-point-testing/ATMlog.xlsx"
 
-        wb = load_workbook(excel_file)
+    source_workbook = load_workbook(excel_file)
 
-        # 创建一个名为“格式化后埋点用例”的新工作表
-        new_sheet = wb.create_sheet(title='测试结果')
+    source_sheet = source_workbook.active
 
-        # 获取所有工作表的名称
-        sheet_names = wb.sheetnames
+    # 加载目标 excel 文件
+    excel_file2 = 'I:/Buried-point-testing/jk埋点.xlsx'
 
-        # 选择查询数据表中的第1个工作表
-        sheet1 = wb[sheet_names[0]]
+    target_workbook = load_workbook(excel_file2)
 
-        # 删除第三列
-        sheet1.delete_cols(3)
+    # 创建一个名为“埋点数据”的新工作表
+ 
+    new_sheet = target_workbook.create_sheet(title="埋点数据")
 
-        # 提取第五列数据
-        first_column = [cell.value for cell in sheet1['E']]
+    target_sheet = target_workbook.sheetnames
 
-        # 保存工作簿，包含整理后的数据
-        output_file = "I:/Buried-point-testing/ATMlog.xlsx"
-        wb.save(output_file)
+    # 选择埋点数据表
+    summary_table_sheet = target_workbook[target_sheet[2]]
 
-
-    def traspose_data():
-
-        excel_file2 = 'I:/Buried-point-testing/jk埋点.xlsx'
-
-        wb2 = load_workbook(excel_file2)
-
-         # 选择查询数据表中的第1个工作表
-        sheet_names = wb2.sheetnames
-
-        sheet1 = wb2[sheet_names[0]]
-
-        # 提取指定列的数据
-        column_indices = [1, 2, 4]
-
-         # 提取第1、2、4列数据
-        extracted_data = []
-        for row in sheet1.iter_rows():
-            row_data = [row[i - 1].value for i in column_indices]
-            extracted_data.append(row_data)
+    # 从源表中取出数据并放置到目标表中
+    for row in source_sheet.iter_rows():
+        row_data = [cell.value for cell in row]
+        summary_table_sheet.append(row_data)
 
 
-    ATMlog_data()
-    traspose_data()
+    # 删除第三列
+    summary_table_sheet.delete_cols(3)
+
+    # 提取第五列数据
+    first_column = [cell.value for col in summary_table_sheet.iter_cols(min_col=5, max_col=5) for cell in col]
+
+
+
+    # 保存工作簿，包含整理后的数据
+    output_file = 'I:/Buried-point-testing/jk埋点.xlsx'
+    target_workbook.save(output_file)
+
+
+#ATMlog_data()
+
+
+def result_table():
+    '''处理查询出来的埋点数据，整理成结果表'''
+    # 加载目标 excel 文件
+    excel_file2 = 'I:/Buried-point-testing/jk埋点.xlsx'
+
+    target_workbook = load_workbook(excel_file2)
+
+    # 创建一个名为“QQ埋点数据”的新工作表
+    new_sheet = target_workbook.create_sheet(title="QQ埋点结果表")
+
+    sheets = target_workbook.sheetnames
+
+    # 选择源数据表和目标数据表
+    source_sheet = target_workbook.worksheets[0]
+    target_sheet = target_workbook.worksheets[3]
+
+    # 指定要选中的列的索引
+    source_column_indices = [1, 2, 4]
+
+    target_column_indices = [1, 2, 3]
+
+    # 将124列的内容放到QQ埋点数据表中
+    for source_index, target_index in zip(source_column_indices, target_column_indices):
+        source_column = [cell[0].value for cell in source_sheet.iter_cols(min_col=source_index, max_col=source_index)]
+        for row, value in enumerate(source_column, start=1):
+            target_sheet.cell(row=row, column=target_index, value=value)
+
+result_table()
+ 
 
     
 
@@ -65,4 +88,4 @@ def copy_arrange_data():
         
 
 
-copy_arrange_data()
+
